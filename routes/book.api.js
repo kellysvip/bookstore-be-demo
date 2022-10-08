98c8310633cd37fad542991717a4bfc862494dee
Kelly <20522051@gm.uit.edu.vn>
2022-10-08 14:11:54 +0700

    update + delete book

--- routes/book.api.js
+++ routes/book.api.js
@@ -140,4 +140,103 @@ router.post("/", (req, res, next) => {
   }
 });
 
+/**
+ * params: /
+ * description: update a book
+ * query:
+ * method: put
+ */
+
+router.put("/:bookId", (req, res, next) => {
+  //put input validation
+  try {
+    const allowUpdate = [
+      "author",
+      "country",
+      "imageLink",
+      "language",
+      "pages",
+      "title",
+      "year",
+    ];
+
+    const { bookId } = req.params;
+
+    const updates = req.body;
+    const updateKeys = Object.keys(updates);
+    //find update request that not allow
+    const notAllow = updateKeys.filter((el) => !allowUpdate.includes(el));
+
+    if (notAllow.length) {
+      const exception = new Error(`Update field not allow`);
+      exception.statusCode = 401;
+      throw exception;
+    }
+    //put processing
+    //Read data from db.json then parse to JSobject
+    let db = fs.readFileSync("db.json", "utf-8");
+    db = JSON.parse(db);
+    const { books } = db;
+    //find book by id
+    const targetIndex = books.findIndex((book) => book.id === bookId);
+    if (targetIndex < 0) {
+      const exception = new Error(`Book not found`);
+      exception.statusCode = 404;
+      throw exception;
+    }
+    //Update new content to db book JS object
+    const updatedBook = { ...db.books[targetIndex], ...updates };
+    db.books[targetIndex] = updatedBook;
+
+    //db JSobject to JSON string
+
+    db = JSON.stringify(db);
+    //write and save to db.json
+    fs.writeFileSync("db.json", db);
+    //put send response
+    res.status(200).send(updatedBook);
+  } catch (error) {
+    next(error);
+  }
+});
+
+/**
+ * params: /
+ * description: update a book
+ * query:
+ * method: delete
+*/
+
+router.delete("/:bookId",(req,res,next)=>{
+    //delete input validation
+    try{
+        const { bookId }=req.params
+        //delete processing
+
+//Read data from db.json then parse to JSobject
+let db = fs.readFileSync("db.json", "utf-8");
+db = JSON.parse(db);
+const { books } = db;
+//find book by id
+const targetIndex=books.findIndex(book=>book.id===bookId)
+if(targetIndex < 0){
+    const exception = new Error(`Book not found`);
+        exception.statusCode = 404;
+        throw exception;
+}
+    //filter db books object
+db.books = books.filter(book=>book.id !== bookId)
+    //db JSobject to JSON string
+
+db=JSON.stringify(db)
+    //write and save to db.json
+
+fs.writeFileSync("db.json",db)
+        //delete send response
+        res.status(200).send({})
+        }catch(error){
+            next(error)
+        }
+    })
+
 module.exports = router;
